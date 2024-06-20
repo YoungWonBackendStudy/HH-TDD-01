e12482b5a61164d40aa8c0b56172f16832909c69
yw7148 <yw7148@naver.com>
2024-06-20 15:03:36 +0000

    예외 케이스 처리 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.hhplus.tdd.database.PointHistoryTable;
@@ -12,32 +11,45 @@ import io.hhplus.tdd.database.UserPointTable;
 
 @Service
 public class PointService {
-
     private static final Logger log = LoggerFactory.getLogger(PointService.class);
-
-    @Autowired
     PointHistoryTable pointHistoryTable;
-
-    @Autowired
     UserPointTable userPointTable;
-
-    public UserPoint getUserPoint(long id) {
-        return userPointTable.selectById(id);
+    
+    public PointService(PointHistoryTable pointHistoryTable, UserPointTable userPointTable) {
+        this.pointHistoryTable = pointHistoryTable;
+        this.userPointTable = userPointTable;
     }
 
-    public List<PointHistory> getPointHistory(long id) {
-        return pointHistoryTable.selectAllByUserId(id);
+    public UserPoint getUserPoint(long userId) {
+        return userPointTable.selectById(userId);
     }
 
-    public UserPoint chargeUserPoint(long id, long amount) {
-        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
-        long currentPoint = userPointTable.selectById(id).point();
-        return userPointTable.insertOrUpdate(id, currentPoint + amount);
+    public List<PointHistory> getPointHistory(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 
-    public UserPoint useUserPoint(long id, long amount) {
-        pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
-        long currentPoint = userPointTable.selectById(id).point();
-        return userPointTable.insertOrUpdate(id, currentPoint - amount);
+    public UserPoint chargeUserPoint(long userId, long chargePoint){
+        if(chargePoint <= 0) 
+            throw new RuntimeException("0이하의 포인트는 충전할 수 없습니다.");
+
+        long currentPoint = userPointTable.selectById(userId).point();
+        UserPoint chargeRes = userPointTable.insertOrUpdate(userId, currentPoint + chargePoint);
+
+        pointHistoryTable.insert(userId, chargePoint, TransactionType.CHARGE, System.currentTimeMillis());
+        return chargeRes;
+    }
+
+    public UserPoint useUserPoint(long userId, long usePoint) {
+        if(usePoint <= 0) 
+            throw new RuntimeException("0보다 적은 포인트는 사용할 수 없습니다.");
+        
+        long currentPoint = userPointTable.selectById(userId).point();
+        if(currentPoint < usePoint)
+            throw new RuntimeException("사용 포인트가 부족합니다.");
+
+        UserPoint useRes = userPointTable.insertOrUpdate(userId, currentPoint - usePoint);
+
+        pointHistoryTable.insert(userId, usePoint, TransactionType.USE, System.currentTimeMillis());
+        return useRes;
     }
 }
